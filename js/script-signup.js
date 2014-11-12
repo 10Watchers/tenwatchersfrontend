c6b934ac3fcfcbff3e2883624f971e37836879e7
Josh Padnick <josh@omedix.com>
2014-11-11 21:27:09 -0800

    Enabled clickable buttons

--- js/script-signup.js
+++ js/script-signup.js
@@ -30,8 +30,28 @@ $(document).ready( function() {
 
     // Handle Events
     // -----------------------------------------------------------------------------------------------------------------
+    $('#btn-1').click( function() {
+        $('#btn-1').addClass('active');
+        $('#btn-2').removeClass('active');
+        $('#btn-3').removeClass('active');
+    });
+
+    $('#btn-2').click( function() {
+        $('#btn-1').removeClass('active');
+        $('#btn-2').addClass('active');
+        $('#btn-3').removeClass('active');
+    });
+
+    $('#btn-3').click( function() {
+        $('#btn-1').removeClass('active');
+        $('#btn-2').removeClass('active');
+        $('#btn-3').addClass('active');
+    });
+
     $('.btn-signUp').on('click', function() {
         window.location.href = "app.html";
     });
+
+
 
 });
